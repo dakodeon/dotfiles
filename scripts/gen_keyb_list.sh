c164e0691f25853c4ada721c4e6670222520ed6a
dakodeon <dakodeon@hotmail.com>
2018-12-23 16:52:08 +0200

    rc.conf and gen_keyb script update

--- scripts/gen_keyb_list.sh
+++ scripts/gen_keyb_list.sh
@@ -2,18 +2,16 @@
 
 # Generate a file with i3 keybindings organized alphabetically
 
-for i in {a..z}; do
-    # grep "+$i " $HOME/dotfiles/i3/config >> $HOME/temp
-    grep "+$i " $HOME/dotfiles/i3/config | while read -r p ; do
-        key=$(echo $p | cut -d' ' -f2)
-        cmd=${p#*$key}
-        echo $key: $cmd >> $HOME/i3binds
-    done
-    printf "\n" >> $HOME/i3binds
-done
+i3conf=$HOME/dotfiles/i3/i3.conf
 
-# while read p; do
-#     key=$(echo $p | cut -d' ' -f2)
-#     cmd=${p#*$key}
-#     echo $key: $cmd
-# done<$HOME/temp
+echo $i3conf
+
+sed -n '/^mode/,/}/d; /bindsym/s/bindsym //p;' $i3conf | # remove all 'mode' sections, and print only 'bindsym' lines, but without 'bindsym'
+    awk '{ c = split($1, a, "+"); # split field 1 (the binding) to substrings, dividing by +
+if ($0 ~ /F[0-9]*\s/) { $0 = "0"a[c]"-"$0 }
+else if ($0 ~ /(Up|Down|Left|Right)\s/) { $0 = "1"a[c]"-"$0 }
+else if ($0 ~ /\+[A-Z][a-z]*\s/) { $0 = "2"a[c]"-"$0 }
+else if ($0 ~ /\+[a-z]{2,}\s/) { $0 = "3"a[c]"-"$0 }
+else { $0 = "4"a[c]"-"$0 }; # all this is used for bindings sorting order: 1) arrow keys, 2) F keys, 3) special keys, 4) descriptive keys, 5) single charcters
+print $0 | "sort" }' | 
+    sed 's/^[a-zA-Z0-9]*-//' # remove sorting changes from the output
