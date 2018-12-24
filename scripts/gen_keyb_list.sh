fe5a81a75d43c4113f5f587cac604c9ef44b6206
dakodeon <dakodeon@hotmail.com>
2018-12-24 18:48:42 +0200

    Keybindings script optimized

--- scripts/gen_keyb_list.sh
+++ scripts/gen_keyb_list.sh
@@ -3,6 +3,7 @@
 # Generate a file with i3 keybindings organized alphabetically
 
 i3conf=$HOME/dotfiles/i3/i3.conf
+rcconf=$HOME/dotfiles/ranger/rc.conf
 
 echo $i3conf
 
@@ -14,4 +15,5 @@ else if ($0 ~ /\+[A-Z][a-z]*\s/) { $0 = "2"a[c]"-"$0 }
 else if ($0 ~ /\+[a-z]{2,}\s/) { $0 = "3"a[c]"-"$0 }
 else { $0 = "4"a[c]"-"$0 }; # all this is used for bindings sorting order: 1) arrow keys, 2) F keys, 3) special keys, 4) descriptive keys, 5) single charcters
 print $0 | "sort" }' | 
-    sed 's/^[a-zA-Z0-9]*-//' # remove sorting changes from the output
+    sed 's/^[a-zA-Z0-9]*-//' | # remove sorting changes from the output
+    awk 'BEGIN { printf ("%20s\t%s\n%20s\t%s\n", "KEY BINDING", "COMMAND", "-----------", "-------") } { bind = $1; $1 = ""; printf ("%20s\t%s\n", bind, $0) }'
