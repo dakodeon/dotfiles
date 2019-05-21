1ef575e7fa5affc3682f0f13ea8b1d0036a9c279
dakodeon <dakodeon@hotmail.com>
2019-05-21 17:30:05 +0300

    now using sxhkd for keybindings

--- scripts/list-keybindings.sh
+++ scripts/list-keybindings.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # Generate a file with i3 keybindings organized alphabetically
 
 i3conf=$HOME/dotfiles/i3/i3.conf
-rcconf=$HOME/dotfiles/ranger/rc.conf
+# rcconf=$HOME/dotfiles/ranger/rc.conf
 
-sed -n '/^mode/,/}/d; /bindsym/s/bindsym //p;' $i3conf | # remove all 'mode' sections, and print only 'bindsym' lines, but without 'bindsym'
+sed -n '/^mode/,/}/d; /bindsym/s/bindsym //p;' "$i3conf" | # remove all 'mode' sections, and print only 'bindsym' lines, but without 'bindsym'
     awk '{ c = split($1, a, "+"); # split field 1 (the binding) to substrings, dividing by +
 if ($0 ~ /F[0-9]*\s/) { $0 = "0"a[c]"-"$0 }
 else if ($0 ~ /(Up|Down|Left|Right)\s/) { $0 = "1"a[c]"-"$0 }
